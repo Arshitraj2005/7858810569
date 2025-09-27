a201a3898264ed8fa6b5342af92cd08a3a02d092
Arshitraj2005 <nigachu789@gmail.com>
2025-09-27 11:23:17 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
+set -e
+
+# Install dependencies
 pip install -r requirements.txt
+
+# Install ffmpeg
+apt-get update && apt-get install -y ffmpeg
+
+# Run main script
 python3 main.py
