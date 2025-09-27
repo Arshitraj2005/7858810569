8445c0b4ae5ce39050c493a7d63f7a79150e2f24
Arshitraj2005 <nigachu789@gmail.com>
2025-09-27 11:13:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
-echo "🚀 Starting YouTube Live Stream service..."
-
-# Run Flask (background me)
-python3 main.py &
-
-# Run Stream
-python3 stream.py
+pip install -r requirements.txt
+python3 main.py
